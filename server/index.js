8efed65b05bad8ff2bb57bc188780980f3d9b3e9
Vlado <wladoandonov@gmail.com>
2025-12-24 23:46:31 +0200

    Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
-import express from 'express'
-import routes from ".routes.js"
+import express, { urlencoded } from 'express';
+import routes from "./routes.js";
+import cors from "cors";
+import mongoose from "mongoose"
+
+try {
+    await mongoose.connect("mongodb://localhost:27017", { dbName: "Finora" });
+    console.log("Connected to DB");
+} catch (err) {
+    console.log("Cannot connect to DB");
+}
 
 const app = express();
 const port = 3000;
 
+app.use(urlencoded({ extended: false }));
+app.use(cors());
 app.use(routes);
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
-    next();
-});
-
 
 app.listen(port, () => {
     console.log(`Server is listening on port http://localhost:${port}`);
