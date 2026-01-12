49424b0e36f35db45b1c6c735facba28d90164c4
Vlado <wladoandonov@gmail.com>
2026-01-12 21:52:17 +0200

    Connected login and register forms to API

--- client/src/views/register.js
+++ client/src/views/register.js
@@ -1,39 +1,68 @@
 import { html } from 'https://esm.run/lit-html@1';
+import { register } from '../api/data.js';
 
-export const registerView = (ctx) =>
-    ctx.render(html`
+export const registerTemplate = (onSubmit, errMessage) =>
+    html`
         <section class="register">
             <div class="register-layout">
-                <form class="register-form" action="/auth/register" method="POST">
+                <form class="register-form" @submit=${onSubmit}>
                     <h2>Sign up now</h2>
 
                     <fieldset>
                         <label for="username">Username</label>
-                        <input type="username" name="username">
+                        <input type="username" name="username" id="username" autocomplete="on">
                     </fieldset>
 
 
                     <fieldset>
                         <label for="email">Email</label>
-                        <input type="email" name="email">
+                        <input type="email" name="email" id="email" autocomplete="on">
                     </fieldset>
 
                     <fieldset>
                         <label for="password">Password</label>
-                        <input type="password" name="password">
+                        <input type="password" name="password" id="password">
                     </fieldset>
 
                     <fieldset>
-                        <label for="re-password">Confirm Password</label>
-                        <input type="password" name="re-password">
+                        <label for="rePassword">Confirm Password</label>
+                        <input type="password" name="rePassword" id="rePassword">
                     </fieldset>
+
+                    <div class="err-message">
+                        <p>${errMessage}</p>
+                    </div>
 
                     <div class="buttons">
                         <button class="sign-up-button">Sign up</button>
                         <p>Or</p>
-                        <button class="sign-in-button">Sign in</button>
+                        <a class="sign-in-link" href="/auth/login">Sign in</a>
                     </div>
                 </form>
             </div>
         </section>
-    `);
+    `;
+
+export async function registerView(ctx, onSubmit) {
+    ctx.render(registerTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const username = formData.get("username").trim()
+        const email = formData.get("email").trim()
+        const password = formData.get("password").trim();
+        const rePassword = formData.get("rePassword").trim();
+
+        if (username === "" || email === "" || password === "" || rePassword === "") {
+            return ctx.render(registerTemplate(onSubmit, "All fields are required!"));
+        }
+        if (password !== rePassword) {
+            return ctx.render(registerTemplate(onSubmit, "Passwords should match!"));
+        }
+
+        await register(email, password);
+
+        ctx.page.redirect("/");
+    }
+}
