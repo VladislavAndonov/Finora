39436084d2032980c3a96172aa28c965aa94ce61
Vlado <wladoandonov@gmail.com>
2026-01-17 16:17:42 +0200

    Added api calls for transactions

--- client/src/api/data.js
+++ client/src/api/data.js
@@ -7,3 +7,35 @@ export const login = api.login;
 export const register = api.register;
 export const logout = api.logout;
 export const verifySession = api.verifySession;
+
+export async function getTransactions() {
+    return await api.get("/transactions");
+}
+
+export async function getLatestTransactions() {
+    return await api.get("/transactions/latest");
+}
+
+export async function getExpenses() {
+    return await api.get("/transactions/expenses");
+}
+
+export async function getIncome() {
+    return await api.get("/transactions/income");
+}
+
+export async function getTransactionById(id) {
+    return await api.get("/transactions/" + id);
+}
+
+export async function addTransaction(data) {
+    return await api.post("/transactions/", data);
+}
+
+export async function editTransaction(data) {
+    return await api.put("/transactions/" + id, data);
+}
+
+export async function deleteTransaction(id) {
+    return await api.del("/transactions/" + id);
+}
