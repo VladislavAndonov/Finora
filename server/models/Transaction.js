0c6311d63b9ce356730649f9e6707655bf3c9966
Vlado <wladoandonov@gmail.com>
2026-02-03 23:47:35 +0200

    Added validation on schema level for User and Transaction

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -1,13 +1,58 @@
 import mongoose from "mongoose";
+
+import { User } from "./User.js";
+
 const { Schema } = mongoose;
 
 const transactionSchema = new Schema({
-    title: { type: String, required: true },
-    ownerId: mongoose.Schema.Types.ObjectId,
-    type: { type: String, enum: ["expenses", "income"], required: true },
-    amount: { type: Number, required: true },
-    date: { type: Date, default: Date.now },
-    category: String,
-})
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+        trim: true,
+        minlength: [3, "Title must be at least 3 characters"],
+        maxlength: [20, "Title must be maximum of 20 characters"],
+    },
+    ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+        required: true,
+        index: true
+    },
+    type: {
+        type: String,
+        enum: {
+            values: ["expenses", "income"],
+            message: "Type can be either expenses or income"
+        },
+        required: [true, "Type is required"],
+        index: true,
+    },
+    amount: {
+        type: Number,
+        required: [true, "Amount is required"],
+        min: [0.01, "Amount should be at least 0.01"],
+        max: [999999.99, "Amount should be maximum of 999,999.99"],
+        validate: {
+            validator: (value) => {
+                return Number.isInteger(value * 100);
+            },
+            message: "Amount can have at most two decimals places"
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        index: true,
+    },
+    category: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        minlength: [3, "Category must be at least 3 characters"],
+        maxlength: [14, "Category must be maximum of 14 characters"],
+    },
+}, { timestamps: true })
+
+transactionSchema.index({ ownerId: 1, date: -1 });
 
 export const Transaction = mongoose.model("Transaction", transactionSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,30 +1,57 @@
 import mongoose from 'mongoose';
 import bcrypt from "bcrypt"
+import config from '../config/config.js';
+import { EMAIL_REGEX } from '../../validators/index.js';
 
-const saltRounds = 10;
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true,
+        minlength: [3, "Username must be at least 3 characters"],
+        maxlength: [14, "Username should be maximum of 14 characters"]
     },
     email: {
         type: String,
-        required: true
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: function (value) {
+                EMAIL_REGEX.test(value)
+            },
+            message: "This is not a valid email format"
+        },
     },
     password: {
         type: String,
-        required: true
+        required: [true, "Password is required"],
+        select: false,
     },
-    balance: { type: Number, default: null }
-});
+    monthlyGoal: {
+        type: Number,
+        min: [10, "Monthly goal should be at least 10"],
+        max: [999999.99, "Monthly goal should be maximum of 999,999.99"],
+        validate: {
+            validator: (value) => {
+                return Number.isInteger(value * 100);
+            },
+            message: "Monthly goal can have at most two decimals places"
+        },
+        default: null
+    }
+}, { timestamps: true });
 
 userSchema.pre("save", async function () {
-    const hash = await bcrypt.hash(this.password, saltRounds);
+    if (!this.isModified("password")) return;
+
+    const hash = await bcrypt.hash(this.password, config.saltRounds);
     this.password = hash;
 });
-
-
 
 export const User = mongoose.model("User", userSchema);
