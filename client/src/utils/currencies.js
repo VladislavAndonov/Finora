87d22522d1e0a8fa33e595e720ff57a585f8a131
Vlado <wladoandonov@gmail.com>
2026-02-24 00:04:44 +0200

    Added currencies table

---
+++ client/src/utils/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    { id: 1, code: "USD", country: "United States", sign: "$" },
+    { id: 2, code: "EUR", country: "Eurozone", sign: "€" },
+    { id: 3, code: "JPY", country: "Japan", sign: "¥" },
+    { id: 4, code: "GBP", country: "United Kingdom", sign: "£" },
+    { id: 5, code: "AUD", country: "Australia", sign: "$" },
+    { id: 6, code: "CAD", country: "Canada", sign: "$" },
+    { id: 7, code: "CHF", country: "Switzerland", sign: "₣" },
+    { id: 8, code: "CNY", country: "China", sign: "¥" },
+    { id: 9, code: "SEK", country: "Sweden", sign: "kr" },
+    { id: 10, code: "NZD", country: "New Zealand", sign: "NZ$" },
+    { id: 11, code: "MXN", country: "Mexico", sign: "$" },
+    { id: 12, code: "INR", country: "India", sign: "₹" },
+    { id: 13, code: "NOK", country: "Norway", sign: "kr" },
+    { id: 14, code: "KRW", country: "South Korea", sign: "₩" },
+    { id: 15, code: "BTC", country: "Decentralized / Digital", sign: "₿" }
+]
