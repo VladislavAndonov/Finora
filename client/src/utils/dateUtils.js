d2a04076690302c912ff5eaeb1a75a57f0282873
Vlado <wladoandonov@gmail.com>
2026-03-10 12:46:50 +0200

    Added format time helper functions

--- client/src/utils/dateUtils.js
+++ client/src/utils/dateUtils.js
@@ -8,14 +8,26 @@ export function formatDate(isoString) {
     const date = new Date(isoString)
     const weekday = date.toLocaleDateString('en-GB', {
         weekday: 'long',
-        timeZone: 'UTC'
     });
 
     const dayAndMonth = date.toLocaleDateString('en-GB', {
         day: 'numeric',
         month: 'long',
-        timeZone: 'UTC'
     });
 
     return `${weekday}, ${dayAndMonth}`;
+}
+
+export function formatHour(dateStr) {
+    if (!dateStr) return '12';
+    const d = new Date(dateStr);
+    return String(d.getHours() % 12 || 12).padStart(2, '0');
+}
+export function formatMinute(dateStr) {
+    if (!dateStr) return '00';
+    return String(new Date(dateStr).getMinutes()).padStart(2, '0');
+}
+export function formatAmPm(dateStr) {
+    if (!dateStr) return 'AM';
+    return new Date(dateStr).getHours() >= 12 ? 'PM' : 'AM';
 }
