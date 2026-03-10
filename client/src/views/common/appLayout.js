1d03a8f556ec8a97f1b761ca834616e749a9e11b
Vlado <wladoandonov@gmail.com>
2026-03-10 12:48:19 +0200

    Added a conditional rendering for fab addTransaction button

--- client/src/views/common/appLayout.js
+++ client/src/views/common/appLayout.js
@@ -14,46 +14,51 @@ export const appLayout = ({
     onConfirmLogout,
     onCancelLogout,
     onAddTransaction
-}) => html`
-    <div class="app-layout">
-        <aside class="app-layout__sidebar">
-            <p class="app-layout__username">${username}</p>
-            <nav class="app-layout__nav">
-                <a 
-                href="/"
-                @click=${navigate}
-                class="app-layout__nav-link ${currentPath === "/" ? "app-layout__nav-link--active" : ""}">
-                <i class="fa-solid fa-home app__nav-icon"></i>
-                Home
-                </a>
-                <a
-                    href="/transactions"
-                    @click=${navigate} 
-                    class="app-layout__nav-link ${currentPath === "/transactions" ? "app-layout__nav-link--active" : ""}">
-                <i class="fa-solid fa-list app__nav-icon"></i>
-                Transactions
-                </a>
-                <a 
-                    href="/calendar"
-                    @click=${navigate} 
-                    class="app-layout__nav-link ${currentPath === "/calendar" ? "app-layout__nav-link--active" : ""}">
-                <i class="fa-regular fa-calendar-days app__nav-icon"></i>
-                Calendar
-                </a>
-                <button @click=${onLogoutClick} class="app-layout__nav-link">
-                <i class="fa-solid fa-right-from-bracket app__nav-icon"></i>
-                Logout
-                </button>
-            </nav>
-        </aside>
+}) => {
+    const hiddenFabRoutes = ['/add', '/edit/'];
+    const showFab = !hiddenFabRoutes.some(route => currentPath.includes(route));
 
-        <main class="app-layout__main">
-            ${content}
-            ${currentPath !== "/transactions/add" && !currentPath.startsWith("/transactions/edit/") ? html`<button @click=${onAddTransaction} class="app-layout__fab" title="Add Transaction">+</button>` : ""} 
-        </main>
+    return html`
+        <div class="app-layout">
+            <aside class="app-layout__sidebar">
+                <p class="app-layout__username">${username}</p>
+                <nav class="app-layout__nav">
+                    <a 
+                    href="/"
+                    @click=${navigate}
+                    class="app-layout__nav-link ${currentPath === "/" ? "app-layout__nav-link--active" : ""}">
+                    <i class="fa-solid fa-home app__nav-icon"></i>
+                    Home
+                    </a>
+                    <a
+                        href="/transactions"
+                        @click=${navigate} 
+                        class="app-layout__nav-link ${currentPath === "/transactions" ? "app-layout__nav-link--active" : ""}">
+                    <i class="fa-solid fa-list app__nav-icon"></i>
+                    Transactions
+                    </a>
+                    <a 
+                        href="/calendar"
+                        @click=${navigate} 
+                        class="app-layout__nav-link ${currentPath === "/calendar" ? "app-layout__nav-link--active" : ""}">
+                    <i class="fa-regular fa-calendar-days app__nav-icon"></i>
+                    Calendar
+                    </a>
+                    <button @click=${onLogoutClick} class="app-layout__nav-link">
+                    <i class="fa-solid fa-right-from-bracket app__nav-icon"></i>
+                    Logout
+                    </button>
+                </nav>
+            </aside>
 
-        ${logoutModalOpen
-        ? modalTemplate({ onConfirmLogout, onCancelLogout })
-        : ""}
-    </div>
-`;
+            <main class="app-layout__main">
+                ${content}
+                ${showFab ? html`<button @click=${onAddTransaction} class="app-layout__fab" title="Add Transaction">+</button>` : ""} 
+            </main>
+
+            ${logoutModalOpen
+            ? modalTemplate({ onConfirmLogout, onCancelLogout })
+            : ""}
+        </div>
+    `;
+}
